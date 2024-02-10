0396edbcf8f79f458f24a29bedccf1fc17b8610a
jjdavenport <jordanjamesdavenport@gmail.com>
2024-02-10 17:49:15 +0000

    Completed Layout.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const passwordInput = document.getElementById("password");
     const confirmPasswordInput = document.getElementById("confirm-password");
+    const errorElement = document.getElementById("error");
 
 
     passwordInput.addEventListener("input", checkPasswords);
@@ -13,13 +14,19 @@ document.addEventListener("DOMContentLoaded", function() {
         const password = passwordInput.value;
         const confirmPassword = confirmPasswordInput.value;
 
-        const errorElement = document.getElementById("error");
+ 
+        if (password === "" || confirmPassword === "") {
+            errorElement.textContent = "*Passwords do not match"; 
+            errorElement.style.color = ""; 
+            return; 
+        }
 
         if (password === confirmPassword) {
             errorElement.textContent = "Passwords match"; 
-            errorElement.style.color = "green";
+            errorElement.style.color = "green"; 
         } else {
             errorElement.textContent = "*Passwords do not match"; 
+            errorElement.style.color = ""; 
         }
     }
-});
+})
